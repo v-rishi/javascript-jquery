27acf0cb9951d6206adab74052479f325730a82a
rishi <rishi@yodaplus.com>
2022-03-28 10:20:28 +0530

    Another commit

---
+++ jquery/progress_bar.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $("#b1").click(function () {
+        $("#progressbar").progressbar({
+            value: 25
+        });
+    });
+});
+$(document).ready(function () {
+    $("#b2").click(function () {
+        $("#progressbar").progressbar({
+            value: 50
+        });
+    });
+});
+$(document).ready(function () {
+    $("#b3").click(function () {
+        $("#progressbar").progressbar({
+            value: 75
+        });
+    });
+});
+$(document).ready(function () {
+    $("#b4").click(function () {
+        $("#progressbar").progressbar({
+            value: 99
+        });
+    });
+});
